99adb5e0bc10b5b7ad13712e2e26b268daf1ad83
KhairuzzamanCP <mdkhariuzzaman@gmail.com>
2023-12-31 23:38:10 +0600

    a

--- mamar_bank/settings.py
+++ mamar_bank/settings.py
@@ -29,9 +29,10 @@ SECRET_KEY = env("SECRET_KEY")
 # SECRET_KEY = 'django-insecure-*p4#3n$&4($^#xomcfs&m7m*t_9hid%ajvl1qals^d6(t=%#2@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ['https://mamar-bank-ersa.onrender.com','https://*.127.0.0.1']
 
 
 # Application definition
